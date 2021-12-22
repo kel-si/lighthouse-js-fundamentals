ddb69fcea2af71488c042755617756b3470337b0
Kelsi <91640967+kel-si@users.noreply.github.com>
2021-12-21 21:01:32 -0700

    Delete age-calculator.js

--- age-calculator.js
+++
@@ -1,6 +0,0 @@
-function ageCalculator(name, yearOfBirth, currentYear) {
-  let age = currentYear - yearOfBirth;
-
-  return name + " is " + age + " years old.";
-}
-console.log(ageCalculator("Alex", 1989, 2021));
