8bc8b08f22cd175f91f81314a0509777bb91064a
Kelsi Olstad <kelsiolstad@gmail.com>
2021-12-21 21:12:27 -0700

    delete file

--- age-calculator.js
+++
@@ -1,6 +0,0 @@
-function ageCalculator(name, yearOfBirth, currentYear) {
-  let age = currentYear - yearOfBirth;
-
-  return name + " is " + age + " years old.";
-}
-console.log(ageCalculator("Taylor Swift", 1989, 2021));
